1ac58b5f4ff2ffd55e382bec52555d938479d745
elray02 <tassofede@gmail.com>
2019-02-25 11:34:02 +0100

    fixing svg draw

--- site.e31bb0bc.js
+++ site.e31bb0bc.js
@@ -27021,7 +27021,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "35505" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "42691" + '/');
 
   ws.onmessage = function (event) {
     var data = JSON.parse(event.data);
