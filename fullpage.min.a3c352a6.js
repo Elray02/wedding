2a5e4ee9b65327f1b23310c9d7364f29f107c49b
elray02 <tassofede@gmail.com>
2019-02-25 11:42:42 +0100

    fixing svg draw

--- fullpage.min.a3c352a6.js
+++ fullpage.min.a3c352a6.js
@@ -204,7 +204,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "42691" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "43253" + '/');
 
   ws.onmessage = function (event) {
     var data = JSON.parse(event.data);
